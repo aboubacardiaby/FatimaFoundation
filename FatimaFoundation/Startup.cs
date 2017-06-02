fec4626226a6d38b0e8f4bdfb3c7e306875f9251
user <user@MNDEV123>
2017-06-02 11:17:23 -0500

    Add project files.

---
+++ FatimaFoundation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FatimaFoundation.Startup))]
+namespace FatimaFoundation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
